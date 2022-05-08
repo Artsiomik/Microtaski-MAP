edaac2107cd3f0bcfbef2135de0620bb9cec3cfc
Artsiom <Artyom.artyom.87@bk.ru>
2022-05-08 21:46:50 +0300

    Updates

---
+++ precache-manifest.7d933aea63eade3df09090ac9c1d1dd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c6b31b3f85a32eb4a4b2e03e1ed278",
+    "url": "/Microtaski-MAP/index.html"
+  },
+  {
+    "revision": "a6c7ce987c7192a722ad",
+    "url": "/Microtaski-MAP/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "f543ebb13449f015479b",
+    "url": "/Microtaski-MAP/static/js/2.c4a9203b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Microtaski-MAP/static/js/2.c4a9203b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c7ce987c7192a722ad",
+    "url": "/Microtaski-MAP/static/js/main.75408936.chunk.js"
+  },
+  {
+    "revision": "65ec1f84dd98f9c983f2",
+    "url": "/Microtaski-MAP/static/js/runtime-main.b8f54283.js"
+  }
+]);
